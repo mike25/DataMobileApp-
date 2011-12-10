48813a50009d8cd9baedb155c98d864626e0a053
R. Robson <rz.robsn@gmail.com>
2011-12-10 18:13:41 -0500

    added GHUnit test feature (rather than SemTest).

---
+++ DataMobileTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DataMobileTests
+//
+//  Created by Kim Sawchuk on 11-12-10.
+//  Copyright (c) 2011 MML-Concordia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
+    }
+}
