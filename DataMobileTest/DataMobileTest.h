588fbb0492b18bb41b3d784e06233ae77c67b02a
R. Robson <rz.robsn@gmail.com>
2011-12-10 19:42:46 -0500

    Revert "Revert "added id generation feature.""
    This reverts commit 61f77a7efce87781a77e48d74cd599633cc5054c.
    
    Reverted back to Semtest because GHUnit wouldn't link.

---
+++ DataMobileTest/DataMobileTest.h
@@ -0,0 +1,13 @@
+//
+//  DataMobileTest.h
+//  DataMobileTest
+//
+//  Created by Kim Sawchuk on 11-12-08.
+//  Copyright (c) 2011 MML-Concordia. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DataMobileTest : SenTestCase
+
+@end
