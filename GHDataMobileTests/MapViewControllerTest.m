1663932585f66f405c1922a75de1a48aaca445fc
Raza Robson <rz.robsn@gmail.com>
2012-03-28 17:51:45 -0400

    started improvement task of map route rendering.

---
+++ GHDataMobileTests/MapViewControllerTest.m
@@ -0,0 +1,60 @@
+//
+//  MapViewControllerTest.m
+//  DataMobile
+//
+//  Created by Zachary Patterson on 3/28/12.
+//  Copyright (c) 2012 MML-Concordia. All rights reserved.
+//
+
+#import "MapViewControllerTest.h"
+
+@implementation MapViewControllerTest
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
